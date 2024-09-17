71ac76d2a3ba5074893cc26d0fc741bf3dca4dc7
PatrakeevM <patrakeev.matvej@bk.ru>
2024-09-17 13:45:49 +0300

    Добавил проверку на регистрацию и возможность встроения переводчика

--- bot.js
+++ bot.js
@@ -3,6 +3,7 @@ import { hydrate } from "@grammyjs/hydrate";
 import dotenv from "dotenv";
 
 import { loadUsers, saveUsers } from "./utils/userStorage.js";
+import { translateText } from "./utils/translate.js";
 
 dotenv.config();
 
@@ -25,60 +26,79 @@ bot.api.setMyCommands([
     description: "Создание профиля",
   },
   {
-    command: "translator",
-    description: "Открыть переводчик",
+    command: "choose",
+    description: "Выбор формата занятия",
   },
-  {
-    command: "dictionary",
-    description: "Открыть словарь",
-  },
+  // {
+  //   command: "translator",
+  //   description: "Открыть переводчик",
+  // },
+  // {
+  //   command: "dictionary",
+  //   description: "Открыть словарь",
+  // },
 ]);
 
 // Создание клавиатур
 const level = new InlineKeyboard()
-  .text("Начальный", "level_start")
-  .text("Средний", "level_intermidiate");
+  .text("Начальный 🌱", "level_start")
+  .text("Средний 📏", "level_intermidiate");
 
-const start = new InlineKeyboard()
-  .text("Уроки и упражнения", "lessons")
+const lesson = new InlineKeyboard()
+  .text("Уроки и упражнения 📚✏️", "lessons")
   .row()
-  .text("Игры и викторины", "quiz")
+  .text("Игры и викторины 🎲🧩", "quiz")
   .row()
-  .text("Разговорная практика", "practice")
+  .text("Разговорная практика 🗣️💬", "practice")
   .row()
-  .text("Темы для обсуждения", "topics");
+  .text("Темы для обсуждения 🗨️🤔", "topics");
 
 // Создание команд
 bot.command("start", async (ctx) => {
   await ctx.reply(
-    `Привет Я - English Buddy!
-Для того, чтобы создать профиль, введите команду /registr`
+    `Привет Я - English Buddy! 👋
+Для того, чтобы создать профиль, введите команду /registr
+Если вы уже зарегистрированы, введите команду /choose`
   );
 });
 
 bot.command("registr", async (ctx) => {
   const userId = ctx.from.id;
   if (users[userId]) {
-    await ctx.reply(`Вы уже зарегистрированы как ${users[userId].name}`);
+    await ctx.reply(`Вы уже зарегистрированы как: ${users[userId].name}`);
   } else {
     await ctx.reply(`Давайте создадим профиль!
 Введите ваше имя:`);
   }
 });
 
-bot.command("translator", async (ctx) => {
-  await ctx.reply(`Введите слово, которое хотите перевести:`);
+bot.command("choose", async (ctx) => {
+  await ctx.reply(`Выберите формат занятия:`, {
+    reply_markup: lesson,
+  });
 });
 
-bot.command("dictionary", async (ctx) => {
-  await ctx.reply(`Ваш словарь:`);
-});
+// bot.command("translator", async (ctx) => {
+//   await ctx.reply(`Введите текст, который хотите перевести в формате (текст; язык перевода)
+// Например: ("привет; ru" или "hello; en")`);
+// });
 
-bot.on("message:text", async (ctx) => {
+// bot.command("dictionary", async (ctx) => {
+//   await ctx.reply(`Ваш словарь:`);
+// });
+
+bot.on(":text", async (ctx) => {
   const userId = ctx.from.id;
 
   // Если пользователь еще не зарегистрирован
   if (!users[userId]) {
+    const name = ctx.message.text;
+
+    if (name.length < 2) {
+      await ctx.reply("Имя должно содержать больше двух букв!");
+      return;
+    }
+
     users[userId] = { name: ctx.message.text, level: null };
     saveUsers(users);
     await ctx.reply(
@@ -88,6 +108,21 @@ bot.on("message:text", async (ctx) => {
         reply_markup: level,
       }
     );
+  } else {
+    const messageText = ctx.message.text;
+
+    if (messageText.includes(";")) {
+      const [textToTranslate, lang] = messageText.split(";");
+      const toRussian = lang.trim().toLowerCase() === "ru";
+
+      const translatedText = translateText(textToTranslate.trim(), toRussian);
+
+      if (translatedText) {
+        await ctx.reply(`Перевод: ${translatedText}`);
+      } else {
+        await ctx.reply("Произошла ошибка при переводе");
+      }
+    }
   }
 });
 
@@ -103,7 +138,7 @@ bot.callbackQuery("level_start", async (ctx) => {
 Уровень: <b>${users[userId].level}</b>
 Теперь выберите формат занятия:`,
       {
-        reply_markup: start,
+        reply_markup: lesson,
         parse_mode: "HTML",
       }
     );
@@ -120,11 +155,9 @@ bot.callbackQuery("level_intermidiate", async (ctx) => {
       `Профиль создан! 
 Имя: <b>${users[userId].name}</b> 
 Уровень: <b>${users[userId].level}</b>
-Теперь выберите формат занятия:
-(Это пока весь функционал
-Спасибо за помощь! Чаще улыбайся - тебе идёт)`,
+Теперь выберите формат занятия:`,
       {
-        reply_markup: start,
+        reply_markup: lesson,
         parse_mode: "HTML",
       }
     );
